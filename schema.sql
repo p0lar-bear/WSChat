1b94cee8045d69d3dcbae69ff23fe0f389064357
Chris Burdick <cburdick@odonnellpdc.com>
2014-07-30 13:05:13 -0400

    Updated schema

--- schema.sql
+++ schema.sql
@@ -3,9 +3,25 @@ DROP DATABASE IF EXISTS wschat;
 CREATE DATABASE wschat;
 USE wschat;
 
+CREATE TABLE users (
+       id INT PRIMARY KEY,
+       name VARCHAR(50) UNIQUE NOT NULL,
+       password CHAR(40) NOT NULL,
+       regtime DATETIME NOT NULL
+);
+
 CREATE TABLE messages (
        id INT PRIMARY KEY,
-       user VARCHAR(50),
-       message TEXT,
-       timestamp DATETIME
+       user_id INT NOT NULL,
+       anon_name VARCHAR(50) NULL,
+       message TEXT NOT NULL,
+       timestamp DATETIME,
+       ip INT NOT NULL
+);
+
+CREATE TABLE sessions (
+       id CHAR(40) PRIMARY KEY,
+       user_id INT NOT NULL,
+       last_update DATETIME NOT NULL,
+       created DATETIME NOT NULL
 );
